f44ede3a303bb90781dcbcf629f32660e74b6ca5
Zoe Mbikakeu <n00256791@iadt.ie>
2026-06-12 21:16:36 +0100

    blink_test working

--- initial_testing/blink_test/src/main.cpp
+++ initial_testing/blink_test/src/main.cpp
@@ -1,17 +1,21 @@
 #include <Arduino.h>
 
 #define LED 33
+#define LED_2 13
 
 void setup()
 {
   // put your setup code here, to run once:
-  digitalRead(LED, OUTPUT);
+  pinMode(LED, OUTPUT);
+  pinMode(LED_2, OUTPUT);
 }
 
 void loop()
 {
   digitalWrite(LED, HIGH);
+  digitalWrite(LED_2, LOW);
   delay(500);
   digitalWrite(LED, LOW);
+  digitalWrite(LED_2, HIGH);
   delay(500);
 }
